fe15ac471c5c3f749a67e5270d4999add65902d8
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:10:51 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,12 @@ output "api_management_api_tags" {
   description = "All api_management_api_tag resources"
   value       = azurerm_api_management_api_tag.api_management_api_tags
 }
+output "api_management_api_tags_api_id" {
+  description = "List of api_id values across all api_management_api_tags"
+  value       = [for k, v in azurerm_api_management_api_tag.api_management_api_tags : v.api_id]
+}
+output "api_management_api_tags_name" {
+  description = "List of name values across all api_management_api_tags"
+  value       = [for k, v in azurerm_api_management_api_tag.api_management_api_tags : v.name]
+}
 
